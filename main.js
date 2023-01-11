14c763ca0997938ca28aaac4b37c7cf543a01ce4
Amandeep Kalakar <amankumaradk@gmail.com>
2023-01-11 22:28:41 +0530

    updated

--- main.js
+++ main.js
@@ -10,12 +10,13 @@ let results = 0;
 
 for (let i = 0; i < 225; i++) {
   const square = document.createElement("div");
+  square.setAttribute("id", i);
   grid.appendChild(square);
 }
 
 const squares = Array.from(document.querySelectorAll(".grid div"));
 
-const alienInvaders = [
+var alienInvaders = [
   0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
   30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
 ];
